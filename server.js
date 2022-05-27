f412b96a129f8c790144e967d4d31756a8b2bdab
Levente26 <madi.levente26@gmail.com>
2022-05-27 17:12:45 +0200

    done

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
+app.use(express.json());
+
+// data 
 const posts = [
     {
         username: 'Levi',
@@ -13,9 +19,27 @@ const posts = [
     },
 ];
 
-app.get('/posts', (req, res) => {
-    res.json(posts);
-})
+// middleware
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader.split(' ')[1];
 
+    if (token == null) {
+        return res.sendStatus(401);
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            res.sendStatus(403);
+        }
+
+        req.user = user;
+        next();
+    })
+};
+
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name));
+});
 
 app.listen(3000);
